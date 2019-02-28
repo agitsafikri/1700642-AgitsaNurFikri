40c3f1b21a84c525545a9691430aedc9666d9d20
Agitsa Nur Fikri <agitsa.nurfikri@upi.edu>
2019-02-28 23:17:09 +0700

    Tugas Pemrograman Visual Ilmu Komputer UPI 2017

---
+++ pv03_1700642_AgitsaNurFikri/pv03_1700642_AgitsaNurFikri/Model/Mahasiswa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pv03_1700642_AgitsaNurFikri.Model
+{
+    class Mahasiswa
+    {
+        public int MyProperty { get; set; }
+
+        public Mahasiswa(int id, string nim, string nama)
+        {
+            id = id;
+            nim = nim;
+            nama = nama;
+        }
+
+        public Mahasiswa()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+
+    }
+}
